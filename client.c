6ba850c1b620f0c8c09a7ea14d6f453b8c79b2a0
Giovana <71229661+giovanamalaia@users.noreply.github.com>
2024-10-09 18:15:03 +0000

    mudanca fifo

--- client.c
+++ client.c
@@ -13,24 +13,19 @@ int main() {
     char buffer[BUFFER_SIZE];
     int client_to_server_fd, server_to_client_fd;
 
-    // Entrar com a mensagem
     printf("Digite uma string: ");
     fgets(buffer, BUFFER_SIZE, stdin);
 
-    // Remover o '\n' do final da string
     buffer[strcspn(buffer, "\n")] = '\0';
 
-    // Enviar a mensagem ao servidor
     client_to_server_fd = open(FIFO_CLIENT_TO_SERVER, O_WRONLY);
-    write(client_to_server_fd, buffer, strlen(buffer) + 1); // +1 para incluir o '\0'
+    write(client_to_server_fd, buffer, strlen(buffer) + 1);
     close(client_to_server_fd);
 
-    // Esperar a resposta do servidor
     server_to_client_fd = open(FIFO_SERVER_TO_CLIENT, O_RDONLY);
     read(server_to_client_fd, buffer, BUFFER_SIZE);
     close(server_to_client_fd);
 
-    // Exibir a resposta
     printf("Resposta do servidor: %s\n", buffer);
 
     return 0;
--- server.c
+++ server.c
@@ -20,24 +20,20 @@ int main() {
     char buffer[BUFFER_SIZE];
     int client_to_server_fd, server_to_client_fd;
 
-    // Criar as FIFOs com permissões de leitura e escrita apenas para o dono
     mkfifo(FIFO_CLIENT_TO_SERVER, S_IRUSR | S_IWUSR);
     mkfifo(FIFO_SERVER_TO_CLIENT, S_IRUSR | S_IWUSR);
 
     printf("Servidor rodando em background...\n");
 
     while (1) {
-        // Abrir FIFO para leitura
         client_to_server_fd = open(FIFO_CLIENT_TO_SERVER, O_RDONLY);
         read(client_to_server_fd, buffer, BUFFER_SIZE);
         close(client_to_server_fd);
 
-        // Transformar a string recebida para maiúsculas
         to_uppercase(buffer);
 
-        // Abrir FIFO para escrever de volta a resposta
         server_to_client_fd = open(FIFO_SERVER_TO_CLIENT, O_WRONLY);
-        write(server_to_client_fd, buffer, strlen(buffer) + 1); // +1 para incluir o '\0'
+        write(server_to_client_fd, buffer, strlen(buffer) + 1); 
         close(server_to_client_fd);
     }
 
